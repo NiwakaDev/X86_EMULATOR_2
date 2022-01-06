35b2f6ec5afd8e955f4541eec871b8efe243b866
Tetsuya-Nishikawa <te1823ya@gmail.com>
2022-01-06 15:57:25 +0900

    Pic.hの修正

--- include/Pic.h
+++ include/Pic.h
@@ -2,18 +2,20 @@
 #include "common.h"
 #include "Fifo.h"
 
-#define PIC0_ICW1		0x0020
-#define PIC0_OCW2		0x0020
-#define PIC0_IMR		0x0021
-#define PIC0_ICW2		0x0021 
-#define PIC0_ICW3		0x0021
-#define PIC0_ICW4		0x0021
-#define PIC1_ICW1		0x00a0
-#define PIC1_OCW2		0x00a0
-#define PIC1_IMR		0x00a1
-#define PIC1_ICW2		0x00a1
-#define PIC1_ICW3		0x00a1
-#define PIC1_ICW4		0x00a1
+enum PIC_REGISTERS{
+    PIC0_ICW1	= 	0x0020,
+    PIC0_OCW2	= 	0x0020,
+    PIC0_IMR	=   0x0021,
+    PIC0_ICW2	= 	0x0021, 
+    PIC0_ICW3	= 	0x0021,
+    PIC0_ICW4	= 	0x0021,
+    PIC1_ICW1	= 	0x00a0,
+    PIC1_OCW2	= 	0x00a0,
+    PIC1_IMR	=   0x00a1,
+    PIC1_ICW2	= 	0x00a1,
+    PIC1_ICW3	= 	0x00a1,
+    PIC1_ICW4	= 	0x00a1,
+};
 
 class Kbc;
 class Timer;
