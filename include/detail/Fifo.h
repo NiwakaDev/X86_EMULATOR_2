83a3571c5f495d3bffeef38cba7759904983af77
Tetsuya-Nishikawa <te1823ya@gmail.com>
2022-01-13 21:26:10 +0900

    include/detail/Fifo.hの修正

--- include/detail/Fifo.h
+++ include/detail/Fifo.h
@@ -1,9 +1,7 @@
 #include "../Fifo.h"
 
-using namespace std;
-
 inline template<typename type> bool Fifo<type>::IsEmpty(){
-    lock_guard<mutex> lock(this->fifo_mtx);
+    std::lock_guard<std::mutex> lock(this->fifo_mtx);
     return this->q.empty();
 }
 
@@ -12,14 +10,14 @@ inline template<typename type> type Fifo<type>::Pop(){
     if(this->IsEmpty()){
         return element;
     }
-    lock_guard<mutex> lock(this->fifo_mtx);
+    std::lock_guard<std::mutex> lock(this->fifo_mtx);
     element = this->q.front();
     this->q.pop();
     return element;
 }
 
 inline template<typename type> void Fifo<type>::Push(const type data){
-    lock_guard<mutex> lock(this->fifo_mtx);
+    std::lock_guard<std::mutex> lock(this->fifo_mtx);
     if(this->q.size()==16){
         return;
     }
@@ -32,7 +30,7 @@ inline template<typename type> type Fifo<type>::Front(){//読み込むだけ
     if(this->IsEmpty()){
         return element;
     }
-    lock_guard<mutex> lock(this->fifo_mtx);
+    std::lock_guard<std::mutex> lock(this->fifo_mtx);
     element = this->q.front();
     return element;
 }
