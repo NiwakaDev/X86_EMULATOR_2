9c8502d9941991c372443b486f7b5593571499f1
Tetsuya-Nishikawa <te1823ya@gmail.com>
2022-01-27 01:46:08 +0900

    class Biosのメンバをスマートポインタに変更

--- include/Bios.h
+++ include/Bios.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "common.h"
-const int BIOS_FUNCTION_SIZE = 256;
 
 class Memory;
 class Cpu;
@@ -9,12 +8,15 @@ class Vga;
 class Kbc;
 
 class Bios{
-    private:
-        BiosFunction *bios_functions[BIOS_FUNCTION_SIZE];
-        std::unique_ptr<Object> obj;
     public:
         Bios(const char* const file_name, Vga& vga, Kbc& kbc);
         ~Bios();
         void LoadIpl(const char* const file_name, Memory& mem);
         void CallFunction(Cpu& cpu, Memory& mem, const uint8_t bios_number);
+    private:
+        enum{
+            BIOS_FUNCTION_SIZE = 256,
+        };
+        std::unique_ptr<BiosFunction> bios_functions[BIOS_FUNCTION_SIZE];
+        std::unique_ptr<Object> obj;
 };
--- src/Bios.cpp
+++ src/Bios.cpp
@@ -6,29 +6,25 @@
 const int IPL_SIZE = 512;
 using namespace std;
 
-Bios::~Bios(){
-    for(int i=0; i<BIOS_FUNCTION_SIZE; i++){
-        if(this->bios_functions[i]!=NULL){
-            delete this->bios_functions[i];
-        }
-    }
-}
-
 Bios::Bios(const char* const file_name, Vga& vga, Kbc& kbc){
-    for(int i=0; i<BIOS_FUNCTION_SIZE; i++){
+    for(int i=0; i<Bios::BIOS_FUNCTION_SIZE; i++){
         this->bios_functions[i] = NULL;
     }
-    this->bios_functions[0x10] = new VideoFunction(vga);
-    this->bios_functions[0x11] = new EquipmentListFunction();
-    this->bios_functions[0x12] = new MemoryFunction();
-    this->bios_functions[0x13] = new FloppyFunction(file_name);
-    this->bios_functions[0x15] = new GeneralSystemServicesFunction();
-    this->bios_functions[0x16] = new KeyFunction(kbc);
-    this->bios_functions[0x1A] = new TimerFunction();
+    this->bios_functions[0x10] = make_unique<VideoFunction>(vga);
+    this->bios_functions[0x11] = make_unique<EquipmentListFunction>();
+    this->bios_functions[0x12] = make_unique<MemoryFunction>();
+    this->bios_functions[0x13] = make_unique<FloppyFunction>(file_name);
+    this->bios_functions[0x15] = make_unique<GeneralSystemServicesFunction>();
+    this->bios_functions[0x16] = make_unique<KeyFunction>(kbc);
+    this->bios_functions[0x1A] = make_unique<TimerFunction>();
+}
+
+Bios::~Bios(){
+
 }
 
 void Bios::CallFunction(Cpu& cpu, Memory& mem, const uint8_t bios_number){
-    if(this->bios_functions[bios_number]==NULL)this->obj->Error("Not implemented: 0x%02X at Bios::CallFunction", bios_number);
+    if(this->bios_functions[bios_number].get()==NULL)this->obj->Error("Not implemented: 0x%02X at Bios::CallFunction", bios_number);
     this->bios_functions[bios_number]->Run(cpu, mem);
 }
 
@@ -38,11 +34,9 @@ void Bios::LoadIpl(const char* const file_name, Memory& mem){
     if(!input_file.is_open()){
         this->obj->Error("Error: input_file.open at Bios::LoadIpl");
     }
-    uint8_t* buff = new uint8_t[IPL_SIZE];
+    uint8_t buff[IPL_SIZE];
     input_file.read((char*)buff, IPL_SIZE);
     for(int i=0; i<IPL_SIZE; i++){
         mem.Write(IPL_START_ADDR+i, buff[i]);
     }
-    input_file.close();
-    delete[] buff;
 }
