69a0f9134ad9fe86951bd83ed011d91029c94e0a
Tetsuya-Nishikawa <te1823ya@gmail.com>
2022-02-17 03:39:28 +0900

    Cpu::GetBaseAddrの修正

--- src/Cpu.cpp
+++ src/Cpu.cpp
@@ -352,9 +352,6 @@ uint32_t Cpu::GetLinearStackAddr(){
 }
 
 uint32_t Cpu::GetBaseAddr(SEGMENT_REGISTER register_type){
-    if(!this->IsProtectedMode()){//read mode
-        return this->segment_registers[register_type]->GetData()*16;
-    }
     return this->segment_registers[register_type]->GetBaseAddr();
 }
 
