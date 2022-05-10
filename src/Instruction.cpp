852d335520f31cba420e5be0885c0a65543806d1
niwaka <te1823ya@gmail.com>
2022-05-11 08:25:43 +0900

    CodeD0::Runの修正

--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -659,7 +659,7 @@ CodeD0::CodeD0(string code_name):Instruction(code_name){
 void CodeD0::Run(const Emulator& emu){
     emu.cpu->AddEip(1);
     this->ParseModRM(emu);
-    if(this->instructions[this->modrm.reg_index]==NULL){
+    if(this->instructions[this->modrm.reg_index].get()==NULL){
             this->Error("Not implemented: D0 /%02X at %s::Run", this->modrm.reg_index, this->code_name.c_str());
     }
     this->instructions[this->modrm.reg_index]->SetModRM(&this->modrm, &this->sib);
