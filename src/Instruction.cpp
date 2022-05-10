82dc27178f028057fc8f92fd8e0dc00b7b9def98
niwaka <te1823ya@gmail.com>
2022-05-11 08:18:29 +0900

    Code0F01::Runの修正

--- src/Instruction.cpp
+++ src/Instruction.cpp
@@ -571,7 +571,7 @@ Code0F01::Code0F01(string code_name):Instruction(code_name){
 void Code0F01::Run(const Emulator& emu){
     emu.cpu->AddEip(1);
     this->ParseModRM(emu);
-    if(this->instructions[this->modrm.reg_index]==NULL){
+    if(this->instructions[this->modrm.reg_index].get()==NULL){
             this->Error("Not implemented: 0F 01 /%02X at %s::Run", this->modrm.reg_index, this->code_name.c_str());
     }
     this->instructions[this->modrm.reg_index]->SetModRM(&this->modrm, &this->sib);
