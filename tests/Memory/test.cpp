8e8785ec2c2e81172929656fceb9f2f867189dbd
niwaka <te1823ya@gmail.com>
2022-05-11 21:31:24 +0900

    Memoryクラスのテストコードを修正

--- tests/Memory/test.cpp
+++ tests/Memory/test.cpp
@@ -20,6 +20,7 @@ int main(){
     {   //Test1
         std::cerr << "Test1" << std::endl;
         const int MEM_SIZE = 10;
+        std::cerr << "MEM_SIZE = " << MEM_SIZE << std::endl;
         //サイズチェック
         Memory memory(MEM_SIZE);
         if(!AssertEqual(memory.GetMemSize(), MEM_SIZE)){
@@ -71,5 +72,58 @@ int main(){
         }  
     }
 
+    {   //Test2
+        std::cerr << "Test2" << std::endl;
+        std::cerr << "MEM_SIZE = " << MEM_SIZE << std::endl;
+        //サイズチェック
+        Memory memory(MEM_SIZE);
+        if(!AssertEqual(memory.GetMemSize(), MEM_SIZE)){
+            std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        //読み書きチェック
+        memory.Write(0, (uint8_t)0xFF);
+        if(!AssertEqual(memory.Read8(0), 0xFF)){
+            std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        //範囲外アクセスチェック
+        {
+            uint8_t data = 0xFA;
+            memory.Write(MEM_SIZE, data);
+            if(!AssertEqual(memory.Read8(MEM_SIZE), 0)){
+                std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        {
+            uint16_t data = 0xFAFB;
+            memory.Write(MEM_SIZE, data);
+            if(!AssertEqual(memory.Read16(MEM_SIZE), 0)){
+                std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        {
+            uint32_t data = 0xFAFDFAFB;
+            memory.Write(MEM_SIZE, data);
+            if(!AssertEqual(memory.Read32(MEM_SIZE), 0)){
+                std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        //buffの要素の番地取得テスト
+        if(!AssertEqualBool(true, NULL==memory.GetPointer(MEM_SIZE))){
+            std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+            exit(EXIT_FAILURE);
+        }  
+        if(!AssertEqualBool(true, NULL!=memory.GetPointer(0))){
+            std::cerr << "MEMORY TEST FAILED !!" << std::endl;
+            exit(EXIT_FAILURE);
+        }  
+    }
     std::cerr << "MEMORY TEST SUCCESS !!" << std::endl;
 }
