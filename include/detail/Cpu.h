c6bfcc84954b9c68a615c0f868fd0cd00a304c6d
niwaka <te1823ya@gmail.com>
2022-05-16 20:26:59 +0900

    Cpu::AddEipとSetEipの修正

--- include/detail/Cpu.h
+++ include/detail/Cpu.h
@@ -2,13 +2,13 @@
 
 inline void Cpu::AddEip(uint32_t data){
     this->eip += data;
-    if(!this->IsProtectedMode()){
+    if(this->IsRealMode()){
         this->eip = this->eip & 0x0000FFFF;
     }
 }
 
 inline void Cpu::SetEip(uint32_t addr){
-    if(!this->IsProtectedMode()){
+    if(this->IsRealMode()){
         this->eip = this->eip & 0x0000FFFF;
     }
     this->eip = addr;
