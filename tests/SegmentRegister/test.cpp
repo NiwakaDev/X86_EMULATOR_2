be2ecf391c8fd31e8a1a2b6f8ca6e02243c1abfc
niwaka <te1823ya@gmail.com>
2022-05-22 01:39:27 +0900

    tests/SegmentRegisterの修正

--- tests/SegmentRegister/test.cpp
+++ tests/SegmentRegister/test.cpp
@@ -70,3 +70,43 @@ TEST(SegmentRegisterTest, CheckSetCache1){
     EXPECT_EQ(segment_register.GetDpl(), 3);
 }
 
+TEST(SegmentRegisterTest, CheckSetCache2){
+    uint16_t selector = 0x0000;
+    SegmentRegister segment_register(selector);
+    uint16_t limit_low    = 0xFFFF;
+    uint16_t base_low     = 0xF000;
+    uint8_t  base_mid     = 0x12;
+    uint8_t  access_right = 0x20;
+    uint8_t limit_high    = 0x4F;
+    uint8_t base_high     = 0xF0;
+    GdtGate gdt_gate = {limit_low, base_low, base_mid, access_right, limit_high, base_high};
+    auto MockGetGdtGate = [&](uint16_t data){return &gdt_gate;};
+    auto MockGetLdtGate = [&](uint16_t data){return (GdtGate*)NULL;};
+    uint16_t mock_data  = 0x1000;
+    segment_register.Set(mock_data, MockGetGdtGate, MockGetLdtGate, true);
+    EXPECT_EQ(segment_register.Is32bitsMode(), true);
+    EXPECT_EQ(segment_register.GetLimit(), 0x000FFFFF);
+    EXPECT_EQ(segment_register.GetBaseAddr(), 0xF012F000);
+    EXPECT_EQ(segment_register.GetDpl(), 1);
+}
+
+TEST(SegmentRegisterTest, CheckSetCache3){
+    //MEMO : real modeのテスト
+    uint16_t selector = 0x1000;
+    SegmentRegister segment_register(selector);
+    uint16_t limit_low    = 0xFFFF;
+    uint16_t base_low     = 0xF000;
+    uint8_t  base_mid     = 0x12;
+    uint8_t  access_right = 0x20;
+    uint8_t limit_high    = 0x4F;
+    uint8_t base_high     = 0xF0;
+    GdtGate gdt_gate = {limit_low, base_low, base_mid, access_right, limit_high, base_high};
+    auto MockGetGdtGate = [&](uint16_t data){return &gdt_gate;};
+    auto MockGetLdtGate = [&](uint16_t data){return (GdtGate*)NULL;};
+    uint16_t mock_data  = 0x1000;
+    segment_register.Set(mock_data, MockGetGdtGate, MockGetLdtGate, false);
+    EXPECT_EQ(segment_register.Is32bitsMode(), false);
+    EXPECT_EQ(segment_register.GetLimit(), 0x0000FFFF);
+    EXPECT_EQ(segment_register.GetBaseAddr(), selector*16);
+    EXPECT_EQ(segment_register.GetData(), 0x1000);
+}
