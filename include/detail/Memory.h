00ede39ff56c3122574b825d5ad99c12467c7f9d
niwaka <te1823ya@gmail.com>
2022-06-04 08:10:50 +0900

    Memory.cppの修正

--- include/detail/Memory.h
+++ include/detail/Memory.h
@@ -27,7 +27,11 @@ inline uint16_t Memory::Read16(const uint32_t addr) const {
 
 inline uint32_t Memory::Read32(const uint32_t addr) const {
   if ((mem_size_ - 3) <= addr) {
+    //MEMO : はりぼてOSはメモリーのサイズよりも大きい番地にアクセスすることで、
+    //大きさを推定している。つまり、Read32では例外を投げることができない。
+    //Read8とRead16の例外を廃止にする方がいいかもしれない。
     return 0;
+    //throw std::out_of_range("Invalid addr");
   }
   uint32_t* data = (uint32_t*)(this->buff.get() + addr);
   return *data;
--- tests/Memory/test.cpp
+++ tests/Memory/test.cpp
@@ -4,73 +4,149 @@
 #include <memory>
 using namespace std;
 
-TEST(MemoryTest, CheckRead8_OutOfRange_0){
-    const int size = 1024;
-    //Memory* memory = new Memory(size);
-    auto memory = make_unique<Memory>(size);
+class MemoryTest: public ::testing::Test{
+    protected:
+        virtual void SetUp(){
+            size = 1024;
+            memory   = make_unique<Memory>(size);
+        }
+        unique_ptr<Memory> memory;
+        int size;
+};
+
+TEST_F(MemoryTest, CheckRead8_OutOfRange_0){
     EXPECT_THROW(memory->Read8(size), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead8_OutOfRange_1){
-    const int size = 1024;
-    //Memory* memory = new Memory(size);
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead8_OutOfRange_1){
     EXPECT_THROW(memory->Read8(size+1), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead8_OutOfRange_2){
-    const int size = 1024;
-    //Memory* memory = new Memory(size);
-    auto memory = make_unique<Memory>(size);
-    EXPECT_THROW(memory->Read8(1024*1024*1024), out_of_range);
+TEST_F(MemoryTest, CheckRead8_OutOfRange_2){
+    EXPECT_THROW(memory->Read8(size*size*size), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead8_OutOfRange_3){
-    const int size = 1024;
-    //Memory* memory = new Memory(size);
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead8_OutOfRange_3){
     EXPECT_NO_THROW(memory->Read8(size-1));
 }
 
-TEST(MemoryTest, CheckRead16_OutOfRange_0){
-    const int size = 1024;
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead16_OutOfRange_0){
     EXPECT_THROW(memory->Read16(size), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead16_OutOfRange_1){
-    const int size = 1024;
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead16_OutOfRange_1){
     EXPECT_THROW(memory->Read16(size+1), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead16_OutOfRange_2){
-    const int size = 1024;
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead16_OutOfRange_2){
     EXPECT_THROW(memory->Read16(1024*1024*1024), out_of_range);
 }
 
-TEST(MemoryTest, CheckRead16_OutOfRange_3){
-    const int size = 1024;
-    auto memory = make_unique<Memory>(size);
+TEST_F(MemoryTest, CheckRead16_OutOfRange_3){
     EXPECT_NO_THROW(memory->Read16(size-2));
 }
 
+TEST_F(MemoryTest, CheckRead16_OutOfRange_4){
+    EXPECT_THROW(memory->Read16(size-1), out_of_range);
+}
 
+TEST_F(MemoryTest, CheckRead32_OutOfRange_0){
+    uint32_t addr = size;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), 0);
+}
+
+TEST_F(MemoryTest, CheckRead32_OutOfRange_1){
+    uint32_t addr = size+1;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), 0);
+}
+
+TEST_F(MemoryTest, CheckRead32_OutOfRange_2){
+    uint32_t addr = size-3;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), 0);
+}
+
+TEST_F(MemoryTest, CheckRead32_OutOfRange_3){
+    uint32_t addr = size-2;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), 0);
+}
+
+TEST_F(MemoryTest, CheckRead32_OutOfRange_4){
+    uint32_t addr = size-1;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), 0);
+}
+
+TEST_F(MemoryTest, CheckRead32_OutOfRange_5){
+    uint32_t addr = size-4;
+    uint32_t data = 0xFAFAFAFA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead8_0){
+    uint32_t addr = 0;
+    uint8_t  data = 0xFA; 
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read8(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead8_1){
+    uint32_t addr = size-1;
+    uint8_t  data = 0x1A;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read8(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead16_0){
+    uint32_t addr = 0;
+    uint16_t data = 0x1CAA;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read16(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead16_1){
+    uint32_t addr = size-2;
+    uint16_t data = 0xED23;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read16(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead32_0){
+    uint32_t addr = 0;
+    uint32_t data = 0x12FFC399;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), data);
+}
+
+TEST_F(MemoryTest, CheckRead32_1){
+    uint32_t addr = size-4;
+    uint32_t data = 0xA2CC4F89;
+    memory->Write(addr, data);
+    EXPECT_EQ(memory->Read32(addr), data);
+}
 /***
-TEST(MemoryTest, CheckSize1){
+TEST_F(MemoryTest, CheckSize1){
     const int size = 1024*1024;
     Memory* memory = new Memory(size);
     EXPECT_EQ(memory->GetMemSize(), size);
 }
 
-TEST(MemoryTest, CheckSize2){
+TEST_F(MemoryTest, CheckSize2){
     const int size = 1024*1024*1024;
     Memory* memory = new Memory(size);
     EXPECT_EQ(memory->GetMemSize(), size);
 }
 
-TEST(MemoryTest, CheckOutOfRange0){
+TEST_F(MemoryTest, CheckOutOfRange0){
     uint8_t data = 0xFA;
     int size     = 1024;
     Memory memory(size);
@@ -78,7 +154,7 @@ TEST(MemoryTest, CheckOutOfRange0){
     EXPECT_EQ(memory.Read8(size), 0);
 }
 
-TEST(MemoryTest, CheckOutOfRange1){
+TEST_F(MemoryTest, CheckOutOfRange1){
     uint8_t data = 0xFA;
     int size     = 1024;
     Memory memory(size);
@@ -86,7 +162,7 @@ TEST(MemoryTest, CheckOutOfRange1){
     EXPECT_EQ(memory.Read8(size+1), 0);
 }
 
-TEST(MemoryTest, CheckWriteRead1Byte0){
+TEST_F(MemoryTest, CheckWriteRead1Byte0){
     int size = 1024;
     Memory memory(size);
     const uint8_t data = 0xFF;
@@ -95,7 +171,7 @@ TEST(MemoryTest, CheckWriteRead1Byte0){
     EXPECT_EQ(memory.Read8(addr), data);
 }
 
-TEST(MemoryTest, CheckWriteRead1Byte1){
+TEST_F(MemoryTest, CheckWriteRead1Byte1){
     int size = 1024;
     Memory memory(size);
     const uint8_t data = 0xFF;
@@ -104,7 +180,7 @@ TEST(MemoryTest, CheckWriteRead1Byte1){
     EXPECT_EQ(memory.Read8(addr), data);
 }
 
-TEST(MemoryTest, CheckWriteRead2Byte0){
+TEST_F(MemoryTest, CheckWriteRead2Byte0){
     int size = 1024;
     Memory memory(size);
     const uint16_t data = 0xFBFA;
@@ -113,7 +189,7 @@ TEST(MemoryTest, CheckWriteRead2Byte0){
     EXPECT_EQ(memory.Read16(addr), data);
 }
 
-TEST(MemoryTest, CheckWriteRead2Byte1){
+TEST_F(MemoryTest, CheckWriteRead2Byte1){
     int size = 1024;
     Memory memory(size);
     const uint16_t data = 0xFBFA;
@@ -122,7 +198,7 @@ TEST(MemoryTest, CheckWriteRead2Byte1){
     EXPECT_EQ(memory.Read16(addr), data);
 }
 
-TEST(MemoryTest, CheckWriteRead4Byte0){
+TEST_F(MemoryTest, CheckWriteRead4Byte0){
     int size = 1024;
     Memory memory(size);
     const uint32_t data = 0xFC12FBFA;
@@ -131,7 +207,7 @@ TEST(MemoryTest, CheckWriteRead4Byte0){
     EXPECT_EQ(memory.Read32(addr), data);
 }
 
-TEST(MemoryTest, CheckWriteRead4Byte1){
+TEST_F(MemoryTest, CheckWriteRead4Byte1){
     int size = 1024;
     Memory memory(size);
     const uint32_t data = 0xFC12FBFA;
